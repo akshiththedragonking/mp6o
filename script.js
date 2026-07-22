c5e4987c8997562260ce6fb81ba40ffd63f3cc2e
akshiththedragonking <akshithdragon@gmail.com>
2026-07-22 14:21:56 -0400

    Add JavaScript for Memory Pet website functionality

---
+++ script.js
@@ -0,0 +1,18 @@
+// My Memory Pet Website JavaScript
+
+// Welcome message when the page loads
+window.onload = function () {
+    console.log("Welcome to My Memory Pet!");
+};
+
+
+// Contact form message
+function sendMessage() {
+    alert("Thank you for contacting My Memory Pet! We will reply soon.");
+}
+
+
+// Button hover message
+function showPetMessage() {
+    console.log("Your Memory Pet is ready to play!");
+}
